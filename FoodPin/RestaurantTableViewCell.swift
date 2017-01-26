b64933ca79e5cb014aae5605b499ff21d35a345a
JG <grafjude@badinhs.org>
2017-01-26 08:25:37 -0500

    second run test

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by BHSRam8 on 1/26/17.
+//  Copyright © 2017 Jude Graf. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
